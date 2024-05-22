c9fa8ff3784ee69783c694def94c12eda423763b
Arindam Saha <arindamsaha1507@gmail.com>
2024-05-22 06:36:36 +0100

    Simulation settings specified

--- FabGPDR.py
+++ FabGPDR.py
@@ -1,6 +1,18 @@
 """Module for running FabGPDR tasks."""
 
-from fabsim.base.fab import *
+# pylint: disable=invalid-name
+
+from fabsim.base.fab import (
+    update_environment,
+    load_plugin_env_vars,
+    execute,
+    job,
+    task,
+    with_config,
+    put_configs,
+    add_local_paths,
+    env,
+)
 
 # Add local script, blackbox and template path.
 add_local_paths("FabGPDR")
@@ -8,7 +20,7 @@ add_local_paths("FabGPDR")
 
 @task
 @load_plugin_env_vars("FabGPDR")
-def gpdr(config, script="main.py", **args):
+def gpdr(config, script="compare_dimension_reductions.py", **args):
     """
     parameters:
       - config : [e.g. brent,harrow,ealing,hillingdon]
@@ -18,8 +30,34 @@ def gpdr(config, script="main.py", **args):
     """
     update_environment(args, {"script": script})
     with_config(config)
-    # print(env)
-    # exit()
+
+    set_simulation_args_list(args)
 
     execute(put_configs, config)
-    job(dict(script="single_run", wall_time="0:15:0", memory="4G", label="test"), args)
+    job(
+        {
+            "script": "single_run",
+            "wall_time": "0:15:0",
+            "memory": "4G",
+            "label": "test",
+        },
+        args,
+    )
+
+
+def set_simulation_args_list(*dicts):
+    """Set the simulation arguments list from the given settings."""
+
+    for adict in dicts:
+        for key in env.simulation_args.keys():
+            if key in adict:
+                env.simulation_args[key] = adict[key]
+
+    env.simulation_args_list = ""
+    for key, value in env.simulation_args.items():
+        if isinstance(value, (list)):
+            env.simulation_args_list += "  ".join(value)
+        else:
+            env.simulation_args_list += f" --{key} {value}"
+
+    print("Simulation prepared with args list:", env.simulation_args_list)
