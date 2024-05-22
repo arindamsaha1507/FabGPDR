47a447817dcff4da2b475faf512e405a0d58d003
Arindam Saha <arindamsaha1507@gmail.com>
2024-05-22 08:38:34 +0100

    Ensemble runs

--- FabGPDR.py
+++ FabGPDR.py
@@ -18,37 +18,73 @@ from fabsim.base.fab import (
 add_local_paths("FabGPDR")
 
 
+def submit_job(
+    config,
+    script,
+    label,
+    wall_time="0:15:0",
+    memory="4G",
+    cores=1,
+    **args,
+):
+    """Submit a job to the cluster."""
+    # pylint: disable=too-many-arguments
+
+    execute(put_configs, config)
+
+    job(
+        {
+            "script": script,
+            "wall_time": wall_time,
+            "memory": memory,
+            "cores": cores,
+            "label": label,
+        },
+        args,
+    )
+
+
 @task
 @load_plugin_env_vars("FabGPDR")
-def gpdr(config, script="compare_dimension_reductions.py", **args):
-    """
-    parameters:
-      - config : [e.g. brent,harrow,ealing,hillingdon]
-      - measures : name of measures input YML file
-      - starting_infections : number of infections to seed 20 days prior to simulation start
-      - quicktest : use larger house sizes to reduce simulation initialisation time
-    """
-    update_environment(args, {"script": script})
+def gpdr(config, run_file="compare_dimension_reductions.py", **args):
+    """Single run of the GPDR model."""
+
+    update_environment(args, {"run_file": run_file, "prefix": ""})
     with_config(config)
 
     set_simulation_args_list(args)
 
-    execute(put_configs, config)
-    job(
-        {
-            "script": "single_run",
-            "wall_time": "0:15:0",
-            "memory": "4G",
-            "label": "test",
-        },
-        args,
-    )
+    submit_job(config, "single_run", "test", **args)
+
+
+@task
+@load_plugin_env_vars("FabGPDR")
+def gpdr_ensemble(
+    config,
+    ensemble_size=200,
+    ensemble_parameter="seed",
+    run_file="compare_dimension_reductions.py",
+    **args,
+):
+    """Ensemble run of the GPDR model."""
+
+    prefix = f"#$ -t 1-{ensemble_size}\n"
+    update_environment(args, {"run_file": run_file, "prefix": prefix})
+    with_config(config)
+
+    set_simulation_args_list(args, {"ensemble_parameter": ensemble_parameter})
+
+    submit_job(config, "single_run", "ensemble_test", **args)
 
 
 def set_simulation_args_list(*dicts):
     """Set the simulation arguments list from the given settings."""
 
     for adict in dicts:
+
+        if "ensemble_parameter" in adict:
+            env.simulation_args[adict["ensemble_parameter"]] = "$SGE_TASK_ID"
+
         for key in env.simulation_args.keys():
             if key in adict:
                 env.simulation_args[key] = adict[key]
